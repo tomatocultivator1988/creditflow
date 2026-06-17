9e0fbf93549ae32b9001553f44b868199d39f1e8
tomatocultivator1988 <anjocabalum2004@gmail.com>
2026-06-17 09:16:23 +0800

    fix build: remove debug script, exclude scripts from tsconfig

--- scripts/check-users.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "../src/generated/prisma/client";
-const p = new PrismaClient();
-p.user
-  .findMany()
-  .then((u) => {
-    console.log(JSON.stringify(u.map((x) => ({ name: x.name, email: x.email, role: x.role })), null, 2));
-  })
-  .catch((e) => console.error(e))
-  .finally(() => p.$disconnect());
